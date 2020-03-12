d25e60da171a14e082c2b2ae0d8a11e82e138226
zhiyi.fang <zhiyi.fang@enflame-tech.com>
2020-03-12 09:48:10 +0800

    nothing much

--- memory_optimizer_test.py
+++ memory_optimizer_test.py
@@ -1,22 +1,27 @@
 import tensorflow as tf
 
-device = "/device:cpu:0"
-# device = "/device:XLA_DTU:0"
+# device = "/device:XLA_CPU:0"
+device = "/device:XLA_DTU:0"
 
 
 def memory_optimizer_test():
-    a = tf.random_normal([128, 128, 12], dtype=tf.float32, name='a')
-    b = tf.random_normal([128, 128, 12], dtype=tf.float32, name='b')
-    c = tf.random_normal([128, 128, 12], dtype=tf.float32, name='c')
-    d = tf.add_n([a, b, c], name='d')
+    with tf.device(device):
+        a = tf.random_normal([1000, 1280, 12], dtype=tf.float32, name='morenfang/a')
+        b = tf.random_normal([1000, 1280, 12], dtype=tf.float32, name='b')
+        c = tf.random_normal([1000, 1280, 12], dtype=tf.float32, name='c')
+        # a = tf.zeros([1000, 1280, 12], dtype=tf.float32, name="morenfang/a")
+        # b = tf.zeros([1000, 1280, 12], dtype=tf.float32, name="b")
+        # c = tf.zeros([1000, 1280, 12], dtype=tf.float32, name="c")
+        d = tf.add_n([a, b, c], name='d')
+        e = tf.sqrt(d, name="e")
 
-    sess = tf.Session()
-    writer = tf.summary.FileWriter(logdir="./logs", graph=sess.graph)
-    init = tf.global_variables_initializer()
-    sess.run(init)
-    out = sess.run(d)
-    print(out)
-    writer.close()
+        sess = tf.Session()
+        writer = tf.summary.FileWriter(logdir="./logs", graph=sess.graph)
+        init = tf.global_variables_initializer()
+        sess.run(init)
+        out = sess.run(e)
+        print(out)
+        writer.close()
 
 
 def main():
